fcad7553b04fcc98730a86b7cdbf57033faa8130
mkfs <mkfs@thoughtgang.org>
2010-10-06 05:05:48 -0400

    OPCODES : Basic unit tests written

--- opcodes/lib/Opcodes.rb
+++ opcodes/lib/Opcodes.rb
@@ -8,6 +8,12 @@ module Opcodes
 
   class Disassembler
 
+=begin rdoc
+Create a new disassembler.
+Note: if the option :arch is not specified here or in Disassembler#disasm,
+the generic_print_address method will be used -- which only generates the
+VMA for an instruction.
+=end
     def self.new(args={})
       dis = ext_new(args)
       yield dis if block_given?
@@ -18,6 +24,10 @@ module Opcodes
 Disassemble all bytes in a buffer. This is simply a wrapper for ext_disasm
 that provides a default value for <i>args</i>.
 See ext_disasm.
+Note: the achhitecture of the target must be specified by the user if the
+target is not a BFD::Target. Failure to specify an architecture will
+result in instruction token arrays containing only the VMA of the
+instruction. See Disassemnbler#new for more info.
 =end
     def disasm( target, args={} )
       # Wrapper provides a default option
--- opcodes/tests/ut_opcodes.rb
+++ opcodes/tests/ut_opcodes.rb
@@ -10,6 +10,8 @@ def hex_buf( arr )
   arr.collect{ |i| i.hex }.pack('C*')
 end
 
+# TODO: BFD test, AT&T vs Intel syntax test, etc
+
 class TC_OpcodesModule < Test::Unit::TestCase
 
   def test_nop
@@ -17,18 +19,17 @@ class TC_OpcodesModule < Test::Unit::TestCase
       buf = hex_buf( %w{ 90 90 90 } )
       ops = dis.disasm( buf )
       assert_equal( 3, ops.length )
-      puts ops.inspect
 
       result = dis.disasm_insn( buf )
-      puts result.inspect
-      #assert_equal( 'nop', result[:insn].mnemonic )
+      assert_equal( 'nop', result[:insn].first )
     end
   end
 
   def test_int3
-    Opcodes::Disassembler.new do |dis|
+    Opcodes::Disassembler.new( :arch => 'x86' ) do |dis|
       ops = dis.disasm( hex_buf( %w{ CC CC CC } ) )
       assert_equal( 3, ops.length )
+      assert_equal( 'int3', ops.first[:insn].first.strip )
     end
   end
 
