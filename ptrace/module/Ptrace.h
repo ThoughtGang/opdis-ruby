cc8c983658e28267ac89e1d1acb00e78da48c923
mkfs <mkfs@thoughtgang.org>
2011-05-28 13:45:07 -0400

    PTRACE: Added Hash mapping ptrace command names (as symbols) to their integer equivalent

--- ptrace/module/Ptrace.h
+++ ptrace/module/Ptrace.h
@@ -8,6 +8,31 @@
 #ifndef PTRACE_RUBY_EXTENSION_H
 #define PTRACE_RUBY_EXTENSION_H
 
+#define SZ_PTRACE_TRACEME "traceme"
+#define SZ_PTRACE_PEEKTEXT "peektext"
+#define SZ_PTRACE_PEEKDATA "peekdata"
+#define SZ_PTRACE_PEEKUSR "peekusr"
+#define SZ_PTRACE_POKETEXT "poketext"
+#define SZ_PTRACE_POKEDATA "pokedata"
+#define SZ_PTRACE_POKEUSR "pokeusr"
+#define SZ_PTRACE_GETREGS "getregs"
+#define SZ_PTRACE_GETFPREGS "getfpregs"
+#define SZ_PTRACE_SETREGS "setregs"
+#define SZ_PTRACE_SETFPREGS "setfpregs"
+#define SZ_PTRACE_CONT "cont"
+#define SZ_PTRACE_SYSCALL "syscall"
+#define SZ_PTRACE_SINGLESTEP "singlestep"
+#define SZ_PTRACE_KILL "kill"
+#define SZ_PTRACE_ATTACH "attach"
+#define SZ_PTRACE_DETACH  "detach"
+// #ifdef linux
+#define SZ_PTRACE_GETSIGINFO "getsiginfo"
+#define SZ_PTRACE_SETSIGINFO "setsiginfo"
+#define SZ_PTRACE_SETOPTIONS "setoptions"
+#define SZ_PTRACE_GETEVENTMSG "geteventmsg"
+#define SZ_PTRACE_SYSEMU "sysemu"
+#define SZ_PTRACE_SYSEMU_SINGLESTEP  "sysemu_singlestep"
+
 /* module and class names */
 #define PTRACE_MODULE_NAME "Ptrace"
 
