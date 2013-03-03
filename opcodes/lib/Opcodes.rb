7a27bdc031ea6d3a3ed8c9158c61a66ae55add63
mkfs <mkfs@thoughtgang.org>
2013-03-03 14:12:10 -0500

    OPCODES-RB : Updated rakefile

--- opcodes/lib/Opcodes.rb
+++ opcodes/lib/Opcodes.rb
@@ -24,10 +24,10 @@ VMA for an instruction.
 Disassemble all bytes in a buffer. This is simply a wrapper for ext_disasm
 that provides a default value for <i>args</i>.
 See ext_disasm.
-Note: the achhitecture of the target must be specified by the user if the
+Note: the architecture of the target must be specified by the user if the
 target is not a BFD::Target. Failure to specify an architecture will
 result in instruction token arrays containing only the VMA of the
-instruction. See Disassemnbler#new for more info.
+instruction. See Disassembler#new for more info.
 =end
     def disasm( target, args={} )
       # Wrapper provides a default option
